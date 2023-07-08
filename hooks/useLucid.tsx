51010cb500387f54b88a004070a94dbfbce03126
shito8 <shito.dev@gmail.com>
2023-07-08 00:50:03 -0300

    fixed hardcode policeId

--- hooks/useLucid.tsx
+++ hooks/useLucid.tsx
@@ -8,7 +8,7 @@ import {
 } from "lucid-cardano";
 import { useContext } from "react";
 import { GlobalContext } from "../components/GlobalContext";
-import { CONSTANTS } from "../utils/constants";
+//import { CONSTANTS } from "../utils/constants";
 
 export default function useLucid() {
   const { lucid, walletApi, config } = useContext(GlobalContext);
@@ -26,7 +26,8 @@ export default function useLucid() {
 
     const cBTCMintingPolicy: Script = {
       type: "PlutusV2",
-      script: CONSTANTS.CBTC_MINTING_POLICY[config.network],
+      //script: CONSTANTS.CBTC_MINTING_POLICY[config.network],
+      script: config.cbtcAssetId,
     };
 
     const unit = toUnit(
